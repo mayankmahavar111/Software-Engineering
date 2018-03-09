86097e1f5daacb74dfc4c040473c2c047d35449e
mayankmahavar111 <mayankmahavar111@gmail.com>
2018-03-10 01:14:52 +0530

    Update data.sql

--- project/data.sql
+++ project/data.sql
@@ -1,16 +1,11 @@
-CREATE TABLE if NOT EXISTS address(
-  add_id integer not NULL PRIMARY KEY ,
-  number text ,
-  city text,
-  street text
-);
+
  create table IF NOT EXISTS student (
   student_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
   name TEXT not NULL ,
   phone integer NOT NULL ,
   dob date NOT NULL ,
-   add_id integer NOT NULL ,
-   FOREIGN KEY (add_id) REFERENCES address(add_id)
+   address text NOT NULL
+   
  );
 CREATE TABLE if NOT EXISTS exam(
   exam_no integer NOT NULL PRIMARY KEY ,
@@ -23,7 +18,6 @@ CREATE TABLE if NOT EXISTS record_card(
   record_no integer NOT NULL PRIMARY KEY ,
   subject text NOT NULL ,
   name text,
-  score integer NOT NULL ,
-  FOREIGN KEY (subject) REFERENCES exam(subject)
+  score integer NOT NULL 
 
 );
